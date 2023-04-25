d9e735657768b0f98bd5a9871686825b642efd64
Vaibhav Agarwal <201136@iiitt.ac.in>
2023-04-25 12:19:52 +0530

    Committing TestCase0

--- TestCases/TestCase0/TestCase_FailedTestCode.py
+++ TestCases/TestCase0/TestCase_FailedTestCode.py
@@ -1,7 +1,4 @@
 import unittest
-import sys
-sys.path.append(r'C:\Users\acer\Desktop\study\SEM VI\SE\Assignment2\PasswordValidator\app')
-from password_valid import Password
 class TestPassword(unittest.TestCase):
     def test_password_exists(self):
         pswd=Password()
