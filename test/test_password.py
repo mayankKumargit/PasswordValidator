391507e0a18deb5e87e402b5fae39a69d27b6a89
Vaibhav Agarwal <201136@iiitt.ac.in>
2023-04-25 12:48:39 +0530

    TestCase1 Failed

--- test/test_password.py
+++ test/test_password.py
@@ -5,5 +5,9 @@ from password_valid import Password
 class TestPassword(unittest.TestCase):
     def test_password_exists(self):
         pswd=Password()
+    def test_password_not_empty(self):
+        pswd=Password()
+        result=pswd.isNull('')
+        self.assertIsNotNone(result)
 if __name__ == "__main__":
     unittest.main()
