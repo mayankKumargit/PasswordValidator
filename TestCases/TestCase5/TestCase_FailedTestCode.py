631e3caadd18742ecd8a0f71161d5b3eb669e9f3
mayankKumar <kmayankkr@gmail.com>
2023-05-02 00:14:47 +0530

    committed failed TestCase5

---
+++ TestCases/TestCase5/TestCase_FailedTestCode.py
@@ -0,0 +1,29 @@
+import unittest
+import sys
+sys.path.append(r'C:\Users\acer\Desktop\study\SEM VI\SE\Assignment2\PasswordValidator\app')
+from password_valid import Password
+class TestPassword(unittest.TestCase):
+    def test_password_exists(self):
+        pswd=Password()
+    def test_password_not_empty(self):
+        pswd=Password()
+        result=pswd.isNull('aJsj5Y@d!j')
+        self.assertIsNotNone(result)
+    def test_password_length_checker(self):
+        pswd=Password()
+        result=pswd.checkLength('aJsj5Y@d!j')
+        self.assertEqual(1,result)
+    def test_password_uppercase_checker(self):
+        pswd=Password()
+        result=pswd.checkUppercase('aJsj5Y@d!j')
+        self.assertEqual(1,result)
+    def test_password_lowercase_checker(self):
+        pswd=Password()
+        result=pswd.checkLowercase('aJsj5Y@d!j')
+        self.assertEqual(1,result)
+    def test_password_digit_checker(self):
+        pswd=Password()
+        result=pswd.checkDigit('aJsj5Y@d!j')
+        self.assertEqual(1,result)
+if __name__ == "__main__":
+    unittest.main()
