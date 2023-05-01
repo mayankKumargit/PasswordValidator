55cdcd23544690c840447a089098990e4387b6a5
Vaibhav Agarwal <201136@iiitt.ac.in>
2023-05-01 23:41:33 +0530

    TestCase5 Failed

--- test/test_password.py
+++ test/test_password.py
@@ -7,19 +7,23 @@ class TestPassword(unittest.TestCase):
         pswd=Password()
     def test_password_not_empty(self):
         pswd=Password()
-        result=pswd.isNull('rt5ybhs!')
+        result=pswd.isNull('aJsj5Y@d!j')
         self.assertIsNotNone(result)
     def test_password_length_checker(self):
         pswd=Password()
-        result=pswd.checkLength('afedsc33fd')
+        result=pswd.checkLength('aJsj5Y@d!j')
         self.assertEqual(1,result)
     def test_password_uppercase_checker(self):
         pswd=Password()
-        result=pswd.checkUppercase('aJsj5Y@dj')
+        result=pswd.checkUppercase('aJsj5Y@d!j')
         self.assertEqual(1,result)
     def test_password_lowercase_checker(self):
         pswd=Password()
-        result=pswd.checkLowercase('aJsj5Y@dj')
+        result=pswd.checkLowercase('aJsj5Y@d!j')
+        self.assertEqual(1,result)
+    def test_password_digit_checker(self):
+        pswd=Password()
+        result=pswd.checkDigit('aJsj5Y@d!j')
         self.assertEqual(1,result)
 if __name__ == "__main__":
     unittest.main()
