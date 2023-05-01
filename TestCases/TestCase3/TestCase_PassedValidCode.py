d7e09cc5c4594e0f6d18c4c26787d40e661be1f1
mayankKumar <kmayankkr@gmail.com>
2023-05-01 23:06:07 +0530

    committed passed TestCase3

---
+++ TestCases/TestCase3/TestCase_PassedValidCode.py
@@ -0,0 +1,16 @@
+class Password(object):
+    def isNull(self,password):
+        if(len(password)==0):
+            return None
+        else:
+            return not None
+    def checkLength(self,password):
+        if(len(password)<8 or len(password)>15):
+            return 0
+        else:
+            return 1
+    def checkUppercase(self,password):
+        for i in password:
+            if (i.isupper()):
+                return 1
+        return 0
