a29d62590ed64b194201b33dcc39b449084cd70d
Vaibhav Agarwal <201136@iiitt.ac.in>
2023-05-01 22:57:04 +0530

    TestCase4 Failed

--- test/test_password.py
+++ test/test_password.py
@@ -17,5 +17,9 @@ class TestPassword(unittest.TestCase):
         pswd=Password()
         result=pswd.checkUppercase('aJsj5Y@dj')
         self.assertEqual(1,result)
+    def test_password_lowercase_checker(self):
+        pswd=Password()
+        result=pswd.checkLowercase('aJsj5Y@dj')
+        self.assertEqual(1,result)
 if __name__ == "__main__":
     unittest.main()
